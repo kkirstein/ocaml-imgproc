891c886058aba095ea395619f1f2cffa79499b91
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2020-10-01 19:37:02 +0200

    use a module functor for single & double precision

--- lib/imgops.ml
+++ lib/imgops.ml
@@ -9,76 +9,125 @@
  
 *)
 
-open Owl.Dense.Ndarray
+module type Ndary = sig
+  type arr
 
-(* calculate centroid of 1d array *)
-let centroid ary =
-  let arr = S.to_array ary in
-  let _, v, m =
-    Array.fold_left
-      (fun (i, v, m) p -> (i + 1, v +. (float_of_int i *. p), m +. p))
-      (1, 0.0, 0.0) arr
-  in
-  v /. m
+  type elt = float
 
-(* centroid or first moment of image *)
-let centroid_line ?(orient = `Hor) img =
-  match S.num_dims img with
-  | 2 -> (
-      match orient with
-      | `Hor ->
-          let len = (S.shape img).(1) in
-          let res =
-            S.of_array
-              (S.map_slice ~axis:0
-                 (fun line -> centroid line)
-                 (S.transpose img))
-              [| 1; len |]
-          in
-          Ok res
-      | `Ver ->
-          let len = (S.shape img).(0) in
-          let res =
-            S.of_array
-              (S.map_slice ~axis:0 (fun line -> centroid line) img)
-              [| len; 1 |]
-          in
-          Ok res )
-  | x -> Error (`Invalid_dimensions x)
+  (* val init : int array -> (int -> elt) -> arr
+*)
+  val shape : arr -> int array
 
-(* extract single line from given image *)
-let profile_line ?(orient = `Hor) coord img =
-  match S.num_dims img with
-  | 2 -> (
-      let dims = S.shape img in
-      let w, h = (dims.(1), dims.(0)) in
-      match orient with
-      | `Hor ->
-          if coord < h then Ok (S.get_slice [ [ coord ]; [] ] img)
-          else Error (`Invalid_index coord)
-      | `Ver ->
-          if coord < w then Ok (S.get_slice [ []; [ coord ] ] img)
-          else Error (`Invalid_index coord) )
-  | x -> Error (`Invalid_dimensions x)
+  val num_dims : arr -> int
 
-let avg_line ?(orient = `Hor) ?avg coord img =
-  match S.num_dims img with
-  | 2 -> (
-      let dims = S.shape img in
-      let w, h = (dims.(1), dims.(0)) in
-      match avg with
-      | Some s when s > 0 -> (
-          let min, max = (coord - (s / 2), coord + (s / 2)) in
-          match orient with
-          | `Hor ->
-              if min < 0 || max >= h then Error (`Invalid_index coord)
-              else
-                let roi = S.get_slice [ [ min; max ]; [] ] img in
-                Ok (S.mean ~axis:0 roi)
-          | `Ver ->
-              if min < 0 || max >= w then Error (`Invalid_index coord)
-              else
-                let roi = S.get_slice [ []; [ min; max ] ] img in
-                Ok (S.mean ~axis:1 roi) )
-      | _ -> profile_line ~orient coord img )
-  | x -> Error (`Invalid_dimensions x)
+  val get_slice : int list list -> arr -> arr
+
+  val mean : ?axis:int -> arr -> arr
+
+  val to_array : arr -> elt array
+
+  val of_array : elt array -> int array -> arr
+
+  val map_slice : ?axis:int -> (arr -> 'c) -> arr -> 'c array
+
+  val transpose : ?axis:int array -> arr -> arr
+end
+
+module type Ops = sig
+  type ary_type
+
+  val centroid_line :
+    ?orient:[< `Hor | `Ver > `Hor ] ->
+    ary_type ->
+    (ary_type, [> `Invalid_dimensions of int ]) result
+
+  val profile_line :
+    ?orient:[< `Hor | `Ver > `Hor ] ->
+    int ->
+    ary_type ->
+    (ary_type, [> `Invalid_dimensions of int | `Invalid_index of int ]) result
+
+  val avg_line :
+    ?orient:[< `Hor | `Ver > `Hor ] ->
+    ?avg:int ->
+    int ->
+    ary_type ->
+    (ary_type, [> `Invalid_dimensions of int | `Invalid_index of int ]) result
+end
+
+module Make (A : Ndary) : Ops with type ary_type := A.arr = struct
+  (* calculate centroid of 1d array *)
+  let centroid ary =
+    let arr = A.to_array ary in
+    let _, v, m =
+      Array.fold_left
+        (fun (i, v, m) p -> (i + 1, v +. (float_of_int i *. p), m +. p))
+        (1, 0.0, 0.0) arr
+    in
+    v /. m
+
+  (* centroid or first moment of image *)
+  let centroid_line ?(orient = `Hor) img =
+    match A.num_dims img with
+    | 2 -> (
+        match orient with
+        | `Hor ->
+            let len = (A.shape img).(1) in
+            let res =
+              A.of_array
+                (A.map_slice ~axis:0
+                   (fun line -> centroid line)
+                   (A.transpose img))
+                [| 1; len |]
+            in
+            Ok res
+        | `Ver ->
+            let len = (A.shape img).(0) in
+            let res =
+              A.of_array
+                (A.map_slice ~axis:0 (fun line -> centroid line) img)
+                [| len; 1 |]
+            in
+            Ok res )
+    | x -> Error (`Invalid_dimensions x)
+
+  (* extract single line from given image *)
+  let profile_line ?(orient = `Hor) coord img =
+    match A.num_dims img with
+    | 2 -> (
+        let dims = A.shape img in
+        let w, h = (dims.(1), dims.(0)) in
+        match orient with
+        | `Hor ->
+            if coord < h then Ok (A.get_slice [ [ coord ]; [] ] img)
+            else Error (`Invalid_index coord)
+        | `Ver ->
+            if coord < w then Ok (A.get_slice [ []; [ coord ] ] img)
+            else Error (`Invalid_index coord) )
+    | x -> Error (`Invalid_dimensions x)
+
+  let avg_line ?(orient = `Hor) ?avg coord img =
+    match A.num_dims img with
+    | 2 -> (
+        let dims = A.shape img in
+        let w, h = (dims.(1), dims.(0)) in
+        match avg with
+        | Some s when s > 0 -> (
+            let min, max = (coord - (s / 2), coord + (s / 2)) in
+            match orient with
+            | `Hor ->
+                if min < 0 || max >= h then Error (`Invalid_index coord)
+                else
+                  let roi = A.get_slice [ [ min; max ]; [] ] img in
+                  Ok (A.mean ~axis:0 roi)
+            | `Ver ->
+                if min < 0 || max >= w then Error (`Invalid_index coord)
+                else
+                  let roi = A.get_slice [ []; [ min; max ] ] img in
+                  Ok (A.mean ~axis:1 roi) )
+        | _ -> profile_line ~orient coord img )
+    | x -> Error (`Invalid_dimensions x)
+end
+
+module S = Make (Owl.Dense.Ndarray.S)
+module D = Make (Owl.Dense.Ndarray.D)
--- lib/imgops.mli
+++ lib/imgops.mli
@@ -9,32 +9,38 @@
   
 *)
 
-open Owl.Dense.Ndarray
+module type Ops = sig
+  type ary_type
 
-val centroid_line :
-  ?orient:[< `Hor | `Ver > `Hor ] ->
-  S.arr ->
-  (S.arr, [> `Invalid_dimensions of int ]) result
-(** [centroid_line ~orient img] calculates centroid values (1. moment) of
+  val centroid_line :
+    ?orient:[< `Hor | `Ver > `Hor ] ->
+    ary_type ->
+    (ary_type, [> `Invalid_dimensions of int ]) result
+  (** [centroid_line ~orient img] calculates centroid values (1. moment) of
     pixel values along the given orientation [orient].
     Returns [`Invalid_dimensions] error, if [img] is not 2 dimensional. *)
 
-val profile_line :
-  ?orient:[< `Hor | `Ver > `Hor ] ->
-  int ->
-  S.arr ->
-  (S.arr, [> `Invalid_dimensions of int | `Invalid_index of int ]) result
-(** [profile_line ~orient coord img] extracts pixel values at [coord] long the given
+  val profile_line :
+    ?orient:[< `Hor | `Ver > `Hor ] ->
+    int ->
+    ary_type ->
+    (ary_type, [> `Invalid_dimensions of int | `Invalid_index of int ]) result
+  (** [profile_line ~orient coord img] extracts pixel values at [coord] long the given
     orientation [orient].
     Returns [`Invalid_dimensions] error, if [img] is not 2 dimensional. *)
 
-val avg_line :
-  ?orient:[< `Hor | `Ver > `Hor ] ->
-  ?avg:int ->
-  int ->
-  S.arr ->
-  (S.arr, [> `Invalid_dimensions of int | `Invalid_index of int ]) result
-(** [avg_line ~orient ?avg coord img] extracts pixel values at [coord] along the given
+  val avg_line :
+    ?orient:[< `Hor | `Ver > `Hor ] ->
+    ?avg:int ->
+    int ->
+    ary_type ->
+    (ary_type, [> `Invalid_dimensions of int | `Invalid_index of int ]) result
+  (** [avg_line ~orient ?avg coord img] extracts pixel values at [coord] along the given
     orientation [orient] and performs additional averaging of pixel values
     along the other dimension.
     Returns [`Invalid_dimensions] error, if [img] is not 2 dimensional. *)
+end
+
+module S : Ops with type ary_type := Owl.Dense.Ndarray.S.arr
+
+module D : Ops with type ary_type := Owl.Dense.Ndarray.D.arr
